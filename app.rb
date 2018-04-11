2366c9cc2824b17c03248801af088d9402f29602
Scott McKell <scott.g.mckell@gmail.com>
2018-04-10 20:06:33 -0600

    created gui

--- app.rb
+++ app.rb
@@ -17,6 +17,16 @@ $message = nil
 get "/" do
 	guess = params["guess"]
 	make_guess(guess) unless guess == nil
+	if $message == "You Win! Making new string!"
+		dic = File.readlines("5desk.txt")
+		choice = dic[rand(dic.length)].downcase
+		until choice.chomp.length >= 5 && choice.chomp.length <= 12
+			choice = dic[rand(dic.length)].downcase
+		end
+		$cpu_choice = choice.chomp
+		$guess_count = 10
+		$choice_string = "_" * $cpu_choice.length
+	end
 	erb :index, :locals => {:cpu_choice => $cpu_choice, :guess_count => $guess_count, :choice_string => $choice_string, :message => $message}
 end
 
@@ -34,7 +44,7 @@ helpers do
 			$guess_count = 10
 			$choice_string = "_" * $cpu_choice.length
 		elsif guess == $cpu_choice
-			$message = "You Win!"
+			$message = "You Win! Making new string!"
 			$choice_string = guess
 		elsif $cpu_choice.include?(guess) && (guess.length == 1)
 			$cpu_choice.each_char.with_index do |x,y|
@@ -42,8 +52,9 @@ helpers do
 					$choice_string[y] = x
 				end
 				if $choice_string.include?("_") == false
-					$message = "You Win!"
+					$message = "You Win! Making new string!"
 				end
+			end
 		elsif ($cpu_choice.include?(guess) == false) && (guess.length == 1)
 			$message = "The word doesn't contain that letter!"
 			$guess_count -= 1
